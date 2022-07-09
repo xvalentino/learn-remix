25c8c4ed2169b50f10c87d296a24cde3df3e9800
Valentino Espinoza <xvalentino13@gmail.com>
2022-07-09 04:45:17 +0000

    wow ok this todo list is working

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,37 @@
 import { Form } from "@remix-run/react";
-import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
 
 type Loaded = { todos?: { name: string }[] };
 
 export const loader: LoaderFunction = async () =>
-  json<Loaded>({ todos: [{ name: "wow" }] });
+  json({ todos: await db.todo.findMany() });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string") {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+  const fields = { name };
+
+  await db.todo.create({ data: fields });
+  return redirect(`/`);
+};
 
 export default function Index() {
   const { todos } = useLoaderData<Loaded>();
   return (
     <div>
       <h1 className="text-xl">Super todo</h1>
-      <Form action="/todos" method="post">
+      <Form method="post">
         <label>
           Name
           <input name="name" type="text"></input>
